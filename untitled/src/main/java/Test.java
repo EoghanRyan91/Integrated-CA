879208f98bc48b10b14d9333f4e23feabd7e5905
EoghanRyan91 <eoghanryan@live.com>
2024-03-31 17:22:42 +0100

    Added functionality to let users choose the desired format for reports

--- untitled/src/main/java/Test.java
+++ untitled/src/main/java/Test.java
@@ -3,7 +3,9 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Test {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/cms";
@@ -12,146 +14,166 @@ public class Test {
 
     public static void main(String[] args) {
         try {
-
+            // Load the MySQL JDBC driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-            // Connect to the database
+
+            // Establish connection to the database
             Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
-            // Generate reports
-            generateCourseReport(connection);
-            generateStudentReport(connection);
-            generateLecturerReport(connection);
+            // Prompt the user to choose the report format
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Choose report format:");
+            System.out.println("1. TXT");
+            System.out.println("2. CSV");
+            System.out.println("3. Console");
+            int choice = scanner.nextInt();
 
-            // Close the connection
+            // Determine the report format based on user's choice
+            ReportFormat format;
+            switch (choice) {
+                case 1:
+                    format = ReportFormat.TXT;
+                    break;
+                case 2:
+                    format = ReportFormat.CSV;
+                    break;
+                case 3:
+                    format = ReportFormat.CONSOLE;
+                    break;
+                default:
+                    System.out.println("Invalid choice, defaulting to console output.");
+                    format = ReportFormat.CONSOLE;
+                    break;
+            }
+
+            // Generate reports based on the chosen format
+            generateCourseReport(connection, format);
+            generateStudentReport(connection, format);
+            generateLecturerReport(connection, format);
+
+            // Close the database connection
             connection.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void generateCourseReport(Connection connection) {
-        try {
-            // Retrieve data for Course Report
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT \n" +
-                    "    modules.module_name AS module_name,\n" +
-                    "    courses.course_programme AS programme,\n" +
-                    "    COUNT(enrollments.student_id) AS num_students_enrolled,\n" +
-                    "    lecturers.lecturer_name AS lecturer,\n" +
-                    "    COALESCE(modules.room, 'online') AS room\n" +
-                    "FROM \n" +
-                    "    modules\n" +
-                    "JOIN \n" +
-                    "    courses ON modules.course_id = courses.course_id\n" +
-                    "LEFT JOIN \n" +
-                    "    enrollments ON modules.module_id = enrollments.module_id\n" +
-                    "LEFT JOIN \n" +
-                    "    lecturers ON modules.lecturer_id = lecturers.lecturer_id\n" +
-                    "GROUP BY \n" +
-                    "    modules.module_id;");
+    // Enum to represent different report formats
+    public enum ReportFormat {
+        TXT, CSV, CONSOLE
+    }
 
-            // Generate Course Report
-            // Write to txt file
-            FileWriter fileWriter = new FileWriter("CourseReport.txt");
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            // Format and write report content
-            while (resultSet.next()) {
-                // Format report content
-                String reportContent = resultSet.getString("module_name") + ", " + resultSet.getString("programme") + ", " +
-                        resultSet.getInt("num_students_enrolled") + ", " + resultSet.getString("lecturer") + ", " +
-                        resultSet.getString("room");
-                // Write report content to txt file
-                printWriter.println(reportContent);
+    // Method to generate course report
+    public static void generateCourseReport(Connection connection, ReportFormat format) {
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT modules.module_name AS module_name, " +
+                     "courses.course_programme AS programme, " +
+                     "COUNT(enrollments.student_id) AS num_students_enrolled, " +
+                     "lecturers.lecturer_name AS lecturer, " +
+                     "COALESCE(modules.room, 'online') AS room " +
+                     "FROM modules " +
+                     "JOIN courses ON modules.course_id = courses.course_id " +
+                     "LEFT JOIN enrollments ON modules.module_id = enrollments.module_id " +
+                     "LEFT JOIN lecturers ON modules.lecturer_id = lecturers.lecturer_id " +
+                     "GROUP BY modules.module_id")) {
+
+            writeReport(resultSet, "CourseReport", format);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to generate student report
+    public static void generateStudentReport(Connection connection, ReportFormat format) {
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT students.student_name, " +
+                     "students.student_number, " +
+                     "courses.course_programme, " +
+                     "GROUP_CONCAT(DISTINCT modules.module_name ORDER BY modules.module_name SEPARATOR ', ') AS enrolled_modules, " +
+                     "GROUP_CONCAT(DISTINCT CONCAT(modules.module_name, ': ', grades.grade) ORDER BY modules.module_name SEPARATOR ', ') AS completed_modules " +
+                     "FROM students " +
+                     "JOIN enrollments ON students.student_id = enrollments.student_id " +
+                     "JOIN modules ON enrollments.module_id = modules.module_id " +
+                     "JOIN courses ON modules.course_id = courses.course_id " +
+                     "LEFT JOIN grades ON enrollments.enrollment_id = grades.enrollment_id " +
+                     "GROUP BY students.student_id, students.student_name, students.student_number, courses.course_programme")) {
+
+            writeReport(resultSet, "StudentReport", format);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to generate lecturer report
+    public static void generateLecturerReport(Connection connection, ReportFormat format) {
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT lecturers.lecturer_name, " +
+                     "lecturers.lecturer_role, " +
+                     "GROUP_CONCAT(DISTINCT modules.module_name ORDER BY modules.module_name SEPARATOR ', ') AS teaching_modules, " +
+                     "COUNT(DISTINCT enrollments.student_id) AS num_students, " +
+                     "lecturers.lecturer_specialization " +
+                     "FROM lecturers " +
+                     "JOIN modules ON lecturers.lecturer_id = modules.lecturer_id " +
+                     "JOIN enrollments ON modules.module_id = enrollments.module_id " +
+                     "GROUP BY lecturers.lecturer_id")) {
+
+            writeReport(resultSet, "LecturerReport", format);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to write report data based on the chosen format
+    public static void writeReport(ResultSet resultSet, String fileName, ReportFormat format) throws SQLException {
+        try {
+            PrintWriter printWriter;
+            if (format == ReportFormat.TXT) {
+                printWriter = new PrintWriter(new FileWriter(fileName + ".txt"));
+                writeTxtReport(resultSet, printWriter);
+            } else if (format == ReportFormat.CSV) {
+                printWriter = new PrintWriter(new FileWriter(fileName + ".csv"));
+                writeCsvReport(resultSet, printWriter);
+            } else {
+                writeConsoleReport(resultSet);
             }
-            printWriter.close();
+            System.out.println("Report has been written to " + fileName);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void generateStudentReport(Connection connection) {
-        try {
-            // Retrieve data for Student Report
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT \n" +
-                    "    students.student_id,\n" +
-                    "    students.student_name,\n" +
-                    "    students.student_number,\n" +
-                    "    courses.course_programme,\n" +
-                    "    GROUP_CONCAT(DISTINCT modules.module_name ORDER BY modules.module_name SEPARATOR ', ') AS enrolled_modules,\n" +
-                    "    GROUP_CONCAT(DISTINCT CONCAT(modules.module_name, ': ', grades.grade) ORDER BY modules.module_name SEPARATOR ', ') AS completed_modules\n" +
-                    "FROM \n" +
-                    "    students\n" +
-                    "JOIN \n" +
-                    "    enrollments ON students.student_id = enrollments.student_id\n" +
-                    "JOIN \n" +
-                    "    modules ON enrollments.module_id = modules.module_id\n" +
-                    "JOIN \n" +
-                    "    courses ON modules.course_id = courses.course_id\n" +
-                    "LEFT JOIN \n" +
-                    "    grades ON enrollments.enrollment_id = grades.enrollment_id\n" +
-                    "GROUP BY \n" +
-                    "    students.student_id,\n" +
-                    "    students.student_name,\n" +
-                    "    students.student_number,\n" +
-                    "    courses.course_programme;\n");
-
-            // Generate Student Report
-            // Write to txt file
-            FileWriter fileWriter = new FileWriter("StudentReport.txt");
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            // Format and write report content
-            while (resultSet.next()) {
-                // Format report content
-                String reportContent = resultSet.getString("student_name") + ", " + resultSet.getString("student_number") + ", " +
-                        resultSet.getString("course_programme") + ", " + resultSet.getString("enrolled_modules") + ", " +
-                        resultSet.getString("completed_modules");
-                // Write report content to txt file
-                printWriter.println(reportContent);
-            }
-            printWriter.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+    // Method to write report data to a text file
+    public static void writeTxtReport(ResultSet resultSet, PrintWriter printWriter) throws SQLException {
+        while (resultSet.next()) {
+            printWriter.println(resultSet.getString(1) + ", " +
+                    resultSet.getString(2) + ", " +
+                    resultSet.getString(3) + ", " +
+                    resultSet.getString(4) + ", " +
+                    resultSet.getString(5));
         }
+        printWriter.close();
     }
 
-    public static void generateLecturerReport(Connection connection) {
-        try {
-            // Retrieve data for Lecturer Report
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT \n" +
-                    "    lecturers.lecturer_name AS lecturer_name,\n" +
-                    "    lecturers.lecturer_role AS lecturer_role,\n" +
-                    "    GROUP_CONCAT(DISTINCT modules.module_name ORDER BY modules.module_name SEPARATOR ', ') AS teaching_modules,\n" +
-                    "    COUNT(DISTINCT enrollments.student_id) AS num_students,\n" +
-                    "    lecturers.lecturer_specialization AS specialization\n" +
-                    "FROM \n" +
-                    "    lecturers\n" +
-                    "JOIN \n" +
-                    "    modules ON lecturers.lecturer_id = modules.lecturer_id\n" +
-                    "JOIN \n" +
-                    "    enrollments ON modules.module_id = enrollments.module_id\n" +
-                    "GROUP BY \n" +
-                    "    lecturers.lecturer_id;\n");
+    // Method to write report data to a CSV file
+    public static void writeCsvReport(ResultSet resultSet, PrintWriter printWriter) throws SQLException {
+        while (resultSet.next()) {
+            printWriter.println(resultSet.getString(1) + "," +
+                    resultSet.getString(2) + "," +
+                    resultSet.getString(3) + "," +
+                    resultSet.getString(4) + "," +
+                    resultSet.getString(5));
+        }
+        printWriter.close();
+    }
 
-            // Generate Lecturer Report
-            // Write to txt file
-            FileWriter fileWriter = new FileWriter("LecturerReport.txt");
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            // Format and write report content
-            while (resultSet.next()) {
-                // Format report content
-                String reportContent = resultSet.getString("lecturer_name") + ", " + resultSet.getString("lecturer_role") + ", " +
-                        resultSet.getString("teaching_modules") + ", " + resultSet.getInt("num_students") + ", " +
-                        resultSet.getString("specialization");
-                // Write report content to txt file
-                printWriter.println(reportContent);
-            }
-            printWriter.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+    // Method to write report data to the console
+    public static void writeConsoleReport(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(1) + ", " +
+                    resultSet.getString(2) + ", " +
+                    resultSet.getString(3) + ", " +
+                    resultSet.getString(4) + ", " +
+                    resultSet.getString(5));
         }
     }
-    // Similar methods for generating Student Report and Lecturer Report
-    // Implement generateStudentReport() and generateLecturerReport() methods
 }
